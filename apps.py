567c74297faa7fce473c92aa11175d2b09b94a4f
Harish B <91817799+HarishB-23@users.noreply.github.com>
2023-03-18 18:09:03 +0530

    Update apps.py

--- apps.py
+++ apps.py
@@ -5,8 +5,6 @@ import requests
 import glob
 import time
 from PIL import Image
-import gtts
-from gtts import gTTS
 import googletrans
 from googletrans import Translator
 from model import get_caption_model, generate_caption
